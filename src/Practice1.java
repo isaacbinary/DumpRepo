174baa0a29bed7a104510078378250c8c26bee8e
HIMANSHU RAWAT <himanshurawat199995@gmail.com>
2022-03-16 14:42:43 +0530

    first commit

---
+++ src/Practice1.java
@@ -0,0 +1,20 @@
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Payment $100 paid"; //one way of creating object for string (pre-defined class present in java package)
+		System.out.println(str.charAt(8)); //single purchase
+		
+		//String str1 = new String("Payment $100 paid"); //other way of creating object
+		//str1.
+		
+		String str1 = "Payments $100 paid";
+		System.out.println(str1.indexOf("$"));
+		System.out.println(str1.substring(9));
+		
+
+	}
+
+}
